ff25db1cfdd2e47652214cc79272168aa3d9e017
Your Name <you@example.com>
2021-09-30 11:32:57 +0200

    további műveletek a Veletlen osztállyal

--- src/hu/petrik/Veletlen.java
+++ src/hu/petrik/Veletlen.java
@@ -1,8 +1,32 @@
 package hu.petrik;
 
-import java.util.Random;
 
-public class Veletlen {
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.Random;
+import java.util.List;
+
+public final class Veletlen {
     private static Random random = new Random();
+    private static List<String> vezetekNevek = feltolt("files/veznev.txt");
+    private static List<String> ferfiNevek = feltolt("files/ferfikernev.txt");
+    private static List<String> noiNevek = feltolt("files/noikernev.txt");
+
     private Veletlen() {}
+
+    private static List<String> feltolt(String fajlnev) {
+
+        List<String> fajlSorai =  new ArrayList<String>();
+        try {
+            fajlSorai = Files.readAllLines(Paths.get(fajlnev));
+
+        } catch (IOException ex) {
+            System.out.println(ex.getMessage());
+        }
+        return fajlSorai;
+    }
+
+
 }
